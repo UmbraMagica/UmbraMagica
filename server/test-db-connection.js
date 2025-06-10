029a4ac7682cd8aacf0d3fad7f45e3366f6361e1
UmbraMagica <umbramagica@gmail.com>
2025-06-10 19:01:10 +0200

    Přidán testovací skript pro ověření připojení k databázi na Renderu

---
+++ server/test-db-connection.js
@@ -0,0 +1,22 @@
+const { Client } = require('pg');
+
+const connectionString = process.env.DATABASE_URL;
+
+const client = new Client({
+  connectionString,
+  ssl: { rejectUnauthorized: false }
+});
+
+client.connect()
+  .then(() => {
+    console.log('✅ Připojení k databázi OK!');
+    return client.query('SELECT NOW()');
+  })
+  .then(res => {
+    console.log('Výsledek dotazu:', res.rows);
+    return client.end();
+  })
+  .catch(err => {
+    console.error('❌ Chyba při připojení:', err);
+    process.exit(1);
+  }); 
