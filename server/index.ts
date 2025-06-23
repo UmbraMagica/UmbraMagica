78ec2f5ea85bb7f768a1f693c6c4437842876c17
Bubli <bubli@example.com>
2025-06-23 10:33:03 +0200

    Popis změn

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import express from "express";
 import cors from "cors";
+import characterInventoryRoutes from "./routes/characterInventory";
 
 const app = express();
 app.set('trust proxy', 1);
